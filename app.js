4d348fc1994a132e49d004247ed2d36156088c41
spectrebugg <spectrebugg@gmail.com>
2020-02-04 10:19:05 -0600

    Update app.js

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const app = express();
 // TODO: Enter the path to your service account json file
 // Need help with this step go here: https://firebase.google.com/docs/admin/setup
 // const serviceAccount = require("./REPLACE_WITH_SERVICE_ACCOUNT.json");
-
+"./madden-league-test-firebase-adminsdk-n06y0-c85e250260.json"
 // TODO: Enter your database url from firebase
 // admin.initializeApp({
 //   credential: admin.credential.cert(serviceAccount),
 //   databaseURL: "https://<DATABASE_NAME>.firebaseio.com/"
-// });
+// });https://madden-league-test.firebaseio.com/
 
 app.set('port', (process.env.PORT || 3001));
 
